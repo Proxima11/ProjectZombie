ec2ae052cfb0dd59f67cf6e21a4b89d7cad7caf2
Lollreurr <c14210186@john.petra.ac.id>
2022-06-09 20:03:44 +0700

    update menu screen kurang start button dll

--- core/src/com/mygdx/game/mainMenuScreen.java
+++ core/src/com/mygdx/game/mainMenuScreen.java
@@ -10,20 +10,17 @@ import com.badlogic.gdx.utils.ScreenUtils;
 import java.awt.*;
 
 public class mainMenuScreen implements Screen {
-    final zombie game;
+    final menuScreen game;
     OrthographicCamera camera;
     public Texture background;
     public Texture play;
     public Rectangle playRec;
 
-    mainMenuScreen(final zombie game) {
+    mainMenuScreen(final menuScreen game) {
         this.game = game;
         background = new Texture(Gdx.files.internal("BackgroundMainMenu.png"));
         play = new Texture(Gdx.files.internal("kayu.png"));
-        playRec.x = 800 / 2 - 200 / 2;
-        playRec.y = 480 - (480 / 3);
-        playRec.width = 200;
-        playRec.height = 100;
+
 
 
         camera = new OrthographicCamera();
@@ -40,9 +37,7 @@ public class mainMenuScreen implements Screen {
         game.batch.setProjectionMatrix(camera.combined);
         game.batch.begin();
         game.batch.draw(background, 0, 0);
-        game.batch.draw(play, playRec.x, playRec.y);
         game.batch.end();
-
     }
 
     @Override
--- desktop/src/com/mygdx/game/DesktopLauncher.java
+++ desktop/src/com/mygdx/game/DesktopLauncher.java
@@ -10,6 +10,6 @@ public class DesktopLauncher {
 		config.setForegroundFPS(60);
 		config.setTitle("Zombie Defender");
 		config.setWindowedMode(800,480);
-		new Lwjgl3Application(new zombie(), config);
+		new Lwjgl3Application(new menuScreen(), config);
 	}
 }

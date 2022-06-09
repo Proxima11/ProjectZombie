dcf941ef11cda01bdf98dbd025854320bcb7a61d
Lollreurr <c14210186@john.petra.ac.id>
2022-06-09 18:54:06 +0700

    ubah ukuran game

--- desktop/src/com/mygdx/game/DesktopLauncher.java
+++ desktop/src/com/mygdx/game/DesktopLauncher.java
@@ -9,6 +9,7 @@ public class DesktopLauncher {
 		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
 		config.setForegroundFPS(60);
 		config.setTitle("Zombie Defender");
+		config.setWindowedMode(800,480);
 		new Lwjgl3Application(new MainZombie(), config);
 	}
 }

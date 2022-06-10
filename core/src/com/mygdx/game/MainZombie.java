d25fc4ae7b8013b3fabb407c7cf77498d52bd600
Lollreurr <c14210186@john.petra.ac.id>
2022-06-10 22:45:10 +0700

    nambahin healthbar

--- core/src/com/mygdx/game/MainZombie.java
+++ core/src/com/mygdx/game/MainZombie.java
@@ -24,6 +24,7 @@ public class MainZombie extends ApplicationAdapter {
     private Texture backgroundGame;
     private Texture police;
     private Texture zombie;
+    private Texture healt100;
     private OrthographicCamera camera;
     private SpriteBatch batch;
     private Rectangle backgroundGameRec;
@@ -38,6 +39,7 @@ public class MainZombie extends ApplicationAdapter {
         backgroundGame = new Texture(Gdx.files.internal("background game2.png"));
         police = new Texture(Gdx.files.internal("police.png"));
         zombie = new Texture(Gdx.files.internal("zombie.png"));
+        healt100 = new Texture(Gdx.files.internal("health bar 100.png"));
         camera = new OrthographicCamera();
         camera.setToOrtho(false, 800, 480);
         batch = new SpriteBatch();
@@ -129,6 +131,7 @@ public class MainZombie extends ApplicationAdapter {
         batch.begin();
         batch.draw(backgroundGame, backgroundGameRec.x, backgroundGameRec.y);
         batch.draw(police, policeRec.x, policeRec.y);
+        batch.draw(healt100, 20, 420);
         for (Rectangle zombies : zombieSpawn) {
             batch.draw(zombie, zombies.x, zombies.y);
         }

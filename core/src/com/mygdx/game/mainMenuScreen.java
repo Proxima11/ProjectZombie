604541fe435f198e621e111bb5d8a02ed33e1a69
Lollreurr <c14210186@john.petra.ac.id>
2022-06-10 12:11:28 +0700

    tambahin menu button

--- core/src/com/mygdx/game/mainMenuScreen.java
+++ core/src/com/mygdx/game/mainMenuScreen.java
@@ -14,12 +14,40 @@ public class mainMenuScreen implements Screen {
     OrthographicCamera camera;
     public Texture background;
     public Texture play;
+    public Texture playNotTouch;
+    public Texture leaderboard;
+    public Texture leaderboardNotTouch;
+    public Texture exit;
+    public Texture exitNotTouch;
     public Rectangle playRec;
+    public Rectangle playRec2;
+    public Rectangle playRec3;
 
     mainMenuScreen(final menuScreen game) {
         this.game = game;
-        background = new Texture(Gdx.files.internal("BackgroundMainMenu.png"));
-        play = new Texture(Gdx.files.internal("kayu.png"));
+        background = new Texture("BackgroundMainMenu.png");
+        play = new Texture("playIs!touch.png");
+        playNotTouch = new Texture("playIsTouch.png");
+        leaderboard = new Texture("leadearboardis!Touch.png");
+        leaderboardNotTouch = new Texture("leadearboardisTouch.png");
+        exit = new Texture("exitis!Touch.png");
+        exitNotTouch = new Texture("exitistouch.png");
+        playRec = new Rectangle();
+        playRec.x = 800 / 2 - 200 / 2;
+        playRec.y = 480 - (160);
+        playRec.width = 200;
+        playRec.height = 100;
+        playRec2 = new Rectangle();
+        playRec2.x = 800 / 2 - 200 / 2;
+        playRec2.y = 480 - (290);
+        playRec2.width = 200;
+        playRec2.height = 100;
+        playRec3 = new Rectangle();
+        playRec3.x = 800 / 2 - 200 / 2;
+        playRec3.y = 480 - (420);
+        playRec3.width = 200;
+        playRec3.height = 100;
+
 
 
 
@@ -33,10 +61,28 @@ public class mainMenuScreen implements Screen {
     public void render(float delta) {
         ScreenUtils.clear(0.2f, 0, 0, 1);
 
+
         camera.update();
         game.batch.setProjectionMatrix(camera.combined);
         game.batch.begin();
         game.batch.draw(background, 0, 0);
+        if (Gdx.input.getX() < playRec.x + playRec.width && Gdx.input.getX() > playRec.x && 480 - Gdx.input.getY() < playRec.y + playRec.height && 480 - Gdx.input.getY() > playRec.y ) {
+            game.batch.draw(playNotTouch, playRec.x, playRec.y);
+        }else{
+            game.batch.draw(play, playRec.x, playRec.y);
+        }
+        if (Gdx.input.getX() < playRec2.x + playRec2.width && Gdx.input.getX() > playRec2.x && 480 - Gdx.input.getY() < playRec2.y + playRec2.height && 480 - Gdx.input.getY() > playRec2.y){
+            game.batch.draw(leaderboardNotTouch,playRec2.x, playRec2.y);
+        }
+        else {
+            game.batch.draw(leaderboard, playRec2.x,playRec2.y);
+        }
+        if (Gdx.input.getX() < playRec3.x + playRec3.width && Gdx.input.getX() > playRec3.x && 480 - Gdx.input.getY() < playRec3.y + playRec3.height && 480 - Gdx.input.getY() > playRec3.y){
+            game.batch.draw(exitNotTouch, playRec3.x, playRec3.y);
+        }
+        else {
+            game.batch.draw(exit, playRec3.x, playRec3.y);
+        }
         game.batch.end();
 
         if(Gdx.input.isTouched()){
